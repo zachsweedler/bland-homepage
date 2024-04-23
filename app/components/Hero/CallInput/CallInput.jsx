118aed764e8a5d55dc049e7f086ec6d959a63ef6
zachsweedler <zachryanco@gmail.com>
2024-04-23 18:51:19 -0400

    welcome call fix

--- app/components/Hero/CallInput/CallInput.jsx
+++ app/components/Hero/CallInput/CallInput.jsx
@@ -1,26 +1,45 @@
 "use client";
-import React, {  useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./callinput.module.css";
 import Button from "@/app/global-components/Button/Button";
 import Phone from "@/app/global-components/PhoneInput/Phone";
 import { PhoneIcon } from "@/app/global-components/Icons/PhoneIcon";
 import { FormProvider, useForm } from "react-hook-form";
-import axios from 'axios'
+import axios from "axios";
 
 function CallInput() {
    const methods = useForm();
    const [loading, setLoading] = useState(false);
+   const [ip, setIP] = useState("");
+
+   useEffect(() => {
+      const getData = async () => {
+         const res = await axios.get("https://api.ipify.org/?format=json");
+         setIP(res.data.ip);
+      };
+      getData();
+   }, []);
+
+   const headers = {
+      "x-real-ip": ip,
+   };
 
    const onSubmit = async (formData) => {
       setLoading(true);
-      await axios.post(
-         "https://app.bland.ai/api/welcome/call",
-         {
-            phone_number: formData.phone_number,
-            firstName: formData.name,
-         },
-         {},
-      );
+      try {
+         await axios.post(
+            "https://app.bland.ai/api/welcome/call",
+            {
+               phoneNumber: formData.phone_number,
+               firstName: formData.name,
+            },
+            {
+               headers,
+            }
+         );
+      } catch (error) {
+         console.error("Error during API call:", error);
+      }
       setLoading(false);
    };
 
