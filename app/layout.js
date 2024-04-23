f71a306f7636fe0383a4f4d57ad093b72bdcadb6
zachsweedler <zachryanco@gmail.com>
2024-04-23 19:13:13 -0400

    meta width

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
          <head>
             <link rel="icon" href="/favicon.ico" sizes="any" />
          </head>
+         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"></meta>
          <html lang="en">
             <ErrorBoundary fallback={<GlobalError />}>
                <body>
