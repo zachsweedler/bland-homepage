183b44724845e41f4e3eeb748d636869437c03fb
zachsweedler <zachryanco@gmail.com>
2024-04-23 18:20:40 -0400

    favicon

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import GlobalError from "./global-error";
 import StyledComponentsRegistry from "@/lib/registry";
 import Nav from "./global-components/Nav/Nav";
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
    title: "Bland AI | Phone Calling Platform",
@@ -11,13 +11,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
    return (
-      <html lang="en">
-         <ErrorBoundary fallback={<GlobalError />}>
-            <body>
-               <Nav/>
-               <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-            </body>
-         </ErrorBoundary>
-      </html>
+      <>
+         <head>
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+         </head>
+         <html lang="en">
+            <ErrorBoundary fallback={<GlobalError />}>
+               <body>
+                  <Nav />
+                  <StyledComponentsRegistry>
+                     {children}
+                  </StyledComponentsRegistry>
+               </body>
+            </ErrorBoundary>
+         </html>
+      </>
    );
 }
